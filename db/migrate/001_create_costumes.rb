caa7c915cbd6d8216c3409330416d6d37c0e08e0
github username <github email address>
2019-09-05 23:54:48 +0100

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,3 +7,16 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+
+class Costumes < ActiveRecord::Migration[5.2]
+    create_table :costumes do |t|
+    def change 
+    t.string = :name
+    t.float = :price
+    t.integer = :price
+    t.string = :image_url 
+    t.datetime = :created_at
+    t.dateime = :updated_at
+    end
+end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,10 @@
 # Create your haunted_houses migration here
+class CreateHaunted_Houses < ActiveRecord::Base
+    def change
+        create_table :costumes do|t|
+        t.string = :name
+        t.string = :location 
+        t.date.time = date.timestamp
+        end
+    end
+end
